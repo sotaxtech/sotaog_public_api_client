c9004df03c3bdc66bd850fde0e6a997b8fbeb146
happyplayer1991 <eryu@sotaog.com>
2023-09-22 03:48:22 -0500

    add facility daily sales list func

--- sotaog_public_api_client/__init__.py
+++ sotaog_public_api_client/__init__.py
@@ -905,6 +905,24 @@ class Client():
       return reports
     else:
       raise Client_Exception('Unable to retrieve custom reports list')
+    
+  def list_facility_daily_sales(self, facility_ids = None, start_date = None, end_date = None):
+    logger.debug('Getting facility based report list')
+    headers = self._get_headers()
+    params = {}
+    if facility_ids:
+      params['facility_ids'] = facility_ids
+    if start_date:
+      params['start_date'] = start_date
+    if end_date:
+      params['end_date'] = end_date
+    result = self.session.get('{}/v1/facilities/sales/daily'.format(self.url), headers=headers, params=params)
+    if result.status_code == 200:
+      reports = result.json()
+      logger.debug('custom reports: {}'.format(reports))
+      return reports
+    else:
+      raise Client_Exception('Unable to retrieve facility based report list')
   
   def list_report_tank_gauge(self, well_ids = None, start_date = None, end_date = None):
     logger.debug('Getting tank gauge report list')
